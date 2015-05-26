cc5a43631e1cf31d88f3da931d4860a024f5399b
Agatio <sebalandre@gmail.com>
2015-05-27 00:15:42 +0200

    Algorithme de Douglas-Peucker
    Simplifie les tracés uploadés (reste à voir comment définir correctement
    epsilon et régler le problème de l'élévation)

--- src/Site/CartoBundle/Controller/MapController.php
+++ src/Site/CartoBundle/Controller/MapController.php
@@ -135,6 +135,7 @@ class MapController extends Controller
 						$elevationString = $elevationString . ";";
 					}
 				}
+                $lsArray = $this->RamerDouglasPeucker($lsArray,0.0015);
 				$ls = new LineString($lsArray);
 				
 				$pog1 = new Point();
@@ -246,7 +247,66 @@ class MapController extends Controller
 	
 	//fonction de modification d'un segment 
 	//séparation d'un segment en deux autre tronçon suivant le points d'insertion.a
+
+    public function perpendicularDistance($ptX, $ptY, $l1x, $l1y, $l2x, $l2y)
+    {
+        $result = 0;
+        if ($l2x == $l1x)
+        {
+            //vertical lines - treat this case specially to avoid divide by zero
+            $result = abs($ptX - $l2x);
+        }
+        else
+        {
+            $slope = (($l2y-$l1y) / ($l2x-$l1x));
+            $passThroughY = (0-$l1x)*$slope + $l1y;
+            $result = (abs(($slope * $ptX) - $ptY + $passThroughY)) / (sqrt($slope*$slope + 1));
+        }
+        return $result;
+    }
+
+    public function RamerDouglasPeucker($pointList, $epsilon)
+    {
+        // Find the point with the maximum distance
+        $dmax = 0;
+        $index = 0;
+        $totalPoints = count($pointList);
+        for ($i = 1; $i < ($totalPoints - 1); $i++)
+        {
+            $d = $this->perpendicularDistance($pointList[$i]->getX(), $pointList[$i]->getY(),
+                                       $pointList[0]->getX(), $pointList[0]->getY(),
+                                       $pointList[$totalPoints-1]->getX(), $pointList[$totalPoints-1]->getY());
+                   
+            if ($d > $dmax)
+            {
+                $index = $i;
+                $dmax = $d;
+            }
+        }
+
+        $resultList = array();
+        
+        // If max distance is greater than epsilon, recursively simplify
+        if ($dmax >= $epsilon)
+        {
+            // Recursive call
+            $recResults1 = $this->RamerDouglasPeucker(array_slice($pointList, 0, $index + 1), $epsilon);
+            $recResults2 = $this->RamerDouglasPeucker(array_slice($pointList, $index, $totalPoints - $index), $epsilon);
+
+            // Build the result list
+            $resultList = array_merge(array_slice($recResults1, 0, count($recResults1) - 1),
+                                      array_slice($recResults2, 0, count($recResults2)));
+        }
+        else
+        {
+            $resultList = array($pointList[0], $pointList[$totalPoints-1]);
+        }
+        // Return the result
+        return $resultList;
+    }
 }
+
+
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
